dcf2d3e274bf50b48d2150c851c68f34d206ed03
Serhii Tochynskyi <tochynskyi.s@gmail.com>
2023-02-11 13:52:24 +0200

    test new branch

--- src/components/user/AuthPage/AuthPage.js
+++ src/components/user/AuthPage/AuthPage.js
@@ -4,7 +4,7 @@ export const AuthPage = () => {
   return (
 	 <div className={styles.container}>
 		<div className={styles.form}>
-			<div className={styles.formTitle}>123</div>
+			<div className={styles.formTitle}>This login page</div>
 		</div>
 	 </div>
   )
